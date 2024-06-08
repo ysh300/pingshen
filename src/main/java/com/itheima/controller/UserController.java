1e6646f0aede5d4de9555e076b666be462d350ff
86131 <1747675890@qq.com>
2024-06-08 20:39:15 +0800

    基础框架

---
+++ src/main/java/com/itheima/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.itheima.controller;
+
+import com.itheima.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class UserController {
+    @Autowired
+    private UserService userService;
+}
---
+++ src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.itheima.service.impl;
+
+import com.itheima.mapper.UserMapper;
+import com.itheima.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserMapper userMapper;
+}
